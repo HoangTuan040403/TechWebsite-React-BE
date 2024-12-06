6ce93bd097b9e0d8daca74d90cbb77a2847f2b70
Tuan <dangvohoangtuan0404@gmail.com>
2024-12-06 20:57:36 +0700

    first commit

---
+++ src/models/UserModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose .Schema(
+    {
+        name: {type: String},
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        isAdmin: {type: Boolean, default: false, require: true},
+        phone: {type: Number},
+        avatar: {type: String}, 
+        address: {type: String},
+        // access_token: {type: String, required: true},
+        // refresh_token: {type: String, required: true},
+    },
+    {
+        timestamps: true
+    }
+);
+const User = mongoose.model("User", userSchema);
+module.exports = User;
